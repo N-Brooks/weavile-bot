b6c960b0a7a5ce969a3c48cc56db7592076c7a51
Matteo AUGER <augermtt@gmail.com>
2019-09-20 21:39:06 +0200

    Updating shibe command: adding dependecy injection

--- src/commands/shibe.ts
+++ src/commands/shibe.ts
@@ -1,15 +1,17 @@
 import { Message } from "discord.js";
-import { injectable } from "inversify";
+import { inject,  injectable } from "inversify";
 import IShibeService from "../services/image/shibe/IShibeService";
 import { Command, ICommand } from "./command";
 
+@injectable()
 @Command("shibe", "Sends a random picture of Shiba Inu", "shibe")
 export class ShibeCommand implements ICommand {
-  constructor(private readonly shibeService: IShibeService)  {
+
+  constructor(@inject("IShibeService") private readonly shibeService: IShibeService) {
   }
 
   public async run(m: Message): Promise<void> {
-    const shibeUrl = this.shibeService.fetchRandomShibe();
+    const shibeUrl = await this.shibeService.fetchRandomShibe();
     m.channel.send(shibeUrl);
   }
 }
