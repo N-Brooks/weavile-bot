ca7836683894da0582d364d37e71189f02ebf79d
Matteo AUGER <augermtt@gmail.com>
2019-09-20 21:47:39 +0200

    Adding comments

--- src/commands/shibe.ts
+++ src/commands/shibe.ts
@@ -3,13 +3,26 @@ import { inject,  injectable } from "inversify";
 import IShibeService from "../services/image/shibe/IShibeService";
 import { Command, ICommand } from "./command";
 
+/**
+ * Shibe command.
+ * Returns a random shiba inu picture on the channel.
+ * @author Matteo AUGER
+ */
 @injectable()
 @Command("shibe", "Sends a random picture of Shiba Inu", "shibe")
 export class ShibeCommand implements ICommand {
 
+  /**
+   * Creates a new instance of ShibeCommand
+   * @param {IShibeService} shibeService Shibe picture service (provided by DI)
+   */
   constructor(@inject("IShibeService") private readonly shibeService: IShibeService) {
   }
 
+ /**
+  * Runs the given command
+  * @param {Message} command command to run
+  */
   public async run(m: Message): Promise<void> {
     const shibeUrl = await this.shibeService.fetchRandomShibe();
     m.channel.send(shibeUrl);
