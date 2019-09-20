09666da017584cb42e41854e7ce18264b1fa82b0
Matteo AUGER <augermtt@gmail.com>
2019-09-20 21:44:21 +0200

    Adding comments

--- src/services/image/shibe/ShibeService.ts
+++ src/services/image/shibe/ShibeService.ts
@@ -2,10 +2,24 @@ import http from "http";
 import { injectable } from "inversify";
 import IShibeService from "./IShibeService";
 
+/**
+ * ShibeService implementation
+ * Fetches a random shiba inu pictures on the internet.
+ * Uses the http://shibe.online/ API.
+ * @author Matteo AUGER
+ */
 @injectable()
 export default class ShibeService implements IShibeService {
+    /**
+     * Random shibe picture url.
+     * Returns a JSON document containing the image at index 0.
+     */
     private readonly url = "http://shibe.online/api/shibes";
 
+    /**
+     * Fetches a random shiba inu pictures on the internet.
+     * @returns {Promise<string>} Random image request promise.
+     */
     public fetchRandomShibe(): Promise<string> {
         return new Promise((resolve, reject) => {
             const request = http.get(this.url, (response) => {
